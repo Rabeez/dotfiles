e08d61de21fe0a4e74c92b5e99055e80685a6fb1
Rabeez Riaz <ruoter123@gmail.com>
2024-09-26 15:40:31 +0400

    Allow telescope to find hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,13 +13,25 @@ return {
 				defaults = {
 					path_display = { "smart" },
 				},
+				pickers = {
+					-- NOTE: This is needed for dotfiles repo since all those files are hidden, but still need to ignore .git
+					find_files = {
+						find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
+					},
+					grep_string = {
+						additional_args = { "--hidden", '--iglob', '!.git', '--case-sensitive', '-w' }
+					},
+					live_grep = {
+						additional_args = { "--hidden", '--iglob', '!.git', '--case-sensitive', '-w' }
+					},
+				},
 			})
 
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Telescope find string under cursor" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-			vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Telescope find string under cursor" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 			vim.keymap.set("n", "<leader>ft", "<Cmd>TodoTelescope<CR>", { desc = "Telescope list TODOs" })
 
