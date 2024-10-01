3014d548f14c66ef82dd562f7b9f33dfe4ccedb8
Rabeez Riaz <ruoter123@gmail.com>
2024-10-01 15:24:51 +0400

    Minor comments

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -61,7 +61,9 @@ return {
 				{ "▏", "FloatBorder" },
 			}
 
+			-- Use custom border by overriding default window_open function
 			local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+			---@diagnostic disable-next-line: duplicate-set-field
 			function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 				opts = opts or {}
 				opts.border = opts.border or border
