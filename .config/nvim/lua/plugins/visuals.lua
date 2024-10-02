f7ef77d4186db7a0fcab4e1a3429dd5db27f8395
Rabeez Riaz <ruoter123@gmail.com>
2024-10-02 17:46:10 +0400

    Fix noice cmp conflicts

--- .config/nvim/lua/plugins/visuals.lua
+++ .config/nvim/lua/plugins/visuals.lua
@@ -130,6 +130,9 @@ return {
 		event = "VeryLazy",
 		opts = {
 			-- add any options here
+			on_open = function(win)
+				vim.api.nvim_win_set_config(win, { focusable = false })
+			end,
 		},
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -141,12 +144,16 @@ return {
 		},
 		config = function()
 			require("noice").setup({
+				-- Disable cmdline suggestions since cmp-cmdline is used instead
+				popupmenu = {
+					enabled = false,
+				},
 				lsp = {
 					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 					override = {
 						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 						["vim.lsp.util.stylize_markdown"] = true,
-						-- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 					},
 				},
 				-- you can enable a preset for easier configuration
@@ -154,11 +161,14 @@ return {
 					bottom_search = true, -- use a classic bottom cmdline for search
 					command_palette = true, -- position the cmdline and popupmenu together
 					long_message_to_split = true, -- long messages will be sent to a split
-					inc_rename = false, -- enables an input dialog for inc-rename.nvim
-					lsp_doc_border = false, -- add a border to hover docs and signature help
+					inc_rename = true, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = true, -- add a border to hover docs and signature help
 				},
 			})
 		end,
+		-- vim.keymap.set("n", "<Esc>", function()
+		-- 	require("notify").dismiss()
+		-- end, { desc = "dismiss notify popup and clear hlsearch" }),
 	},
 	{
 		"folke/zen-mode.nvim",
