68ff275a75f833d52e61d8446fc38a3149ebcc1d
Rabeez Riaz <ruoter123@gmail.com>
2024-10-02 20:46:44 +0400

    Setup rich based rendering for jupyter notebooks in telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,8 +9,55 @@ return {
 		},
 		config = function()
 			local telescope = require("telescope")
+			local previewers = require("telescope.previewers")
+			local new_maker = function(filepath, bufnr, opts)
+				opts = opts or {}
+				if opts.use_ft_detect == nil then
+					opts.use_ft_detect = true
+				end
+				if filepath:match(".*%.ipynb") then
+					vim.schedule(function()
+						-- local tail = require("telescope.utils").path_tail(filepath)
+						local cmd = 'rich "'
+							.. filepath
+							.. '" '
+							.. "--line-numbers "
+							-- NOTE: This outputs ANSI escape codes for colors but telescope just renders 
+							-- the raw characters instead of syntax highlighting
+							-- .. "--force-terminal "
+							.. "--panel=none "
+							.. "--max-width=50 "
+						local handle = io.popen(cmd)
+						if handle == nil then
+							vim.api.nvim_buf_set_lines(
+								bufnr,
+								0,
+								-1,
+								false,
+								{ "ERROR IN RENDERING JUPYTER NOTEBOOK VIA RICH" }
+							)
+						else
+							local function split(string, separator)
+								local tabl = {}
+								for str in string.gmatch(string, "[^" .. separator .. "]+") do
+									table.insert(tabl, str)
+								end
+								return tabl
+							end
+							local result = handle:read("*a")
+							handle:close()
+							vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, split(result, "\n"))
+						end
+					end)
+				else
+					previewers.buffer_previewer_maker(filepath, bufnr, opts)
+				end
+
+				-- previewers.buffer_previewer_maker(filepath, bufnr, opts)
+			end
 			telescope.setup({
 				defaults = {
+					buffer_previewer_maker = new_maker,
 					path_display = { "smart" },
 				},
 				pickers = {
@@ -18,13 +65,13 @@ return {
 					find_files = {
 						-- NOTE: This is to enable color highlights of hidden files in file explorers
 						hidden = true,
-						find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
+						find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
 					},
 					grep_string = {
-						additional_args = { "--hidden", '--iglob', '!.git', '--case-sensitive', '-w' }
+						additional_args = { "--hidden", "--iglob", "!.git", "--case-sensitive", "-w" },
 					},
 					live_grep = {
-						additional_args = { "--hidden", '--iglob', '!.git', '--case-sensitive', '-w' }
+						additional_args = { "--hidden", "--iglob", "!.git", "--case-sensitive", "-w" },
 					},
 				},
 			})
@@ -36,6 +83,7 @@ return {
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 			vim.keymap.set("n", "<leader>ft", "<Cmd>TodoTelescope<CR>", { desc = "Telescope list TODOs" })
+			vim.keymap.set("n", "<leader>fn", "<Cmd>Telescope notify<CR>", { desc = "Telescope list notifications" })
 
 			telescope.load_extension("fzf")
 			telescope.load_extension("notify")
