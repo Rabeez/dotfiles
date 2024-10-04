fb84a8955624598f6e66a53f9f03750cd3d1acd6
Rabeez Riaz <ruoter123@gmail.com>
2024-10-04 14:44:09 +0400

    Move todo comments config and change NOTE color

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -37,4 +37,17 @@ return {
 	{
 		"mbbill/undotree",
 	},
+	{
+		"folke/todo-comments.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			keywords = {
+				NOTE = { color = "#86C3A1" },
+			},
+		},
+	},
 }
--- .config/nvim/lua/plugins/linter-config.lua
+++ .config/nvim/lua/plugins/linter-config.lua
@@ -47,14 +47,4 @@ return {
 			}
 		end,
 	},
-	{
-		"folke/todo-comments.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
 }
