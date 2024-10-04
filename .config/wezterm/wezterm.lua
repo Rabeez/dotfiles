e575bb0855eb74ce875866b05af3f8996b77f011
Rabeez Riaz <ruoter123@gmail.com>
2024-10-04 16:42:11 +0400

    Setup wezterm status bar with mode and workspace

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,9 +2,21 @@ local wezterm = require("wezterm")
 local sessionizer = require("sessionizer")
 local config = wezterm.config_builder()
 
+-- <<< Style elements >>>
+local SOLID_RIGHT_ARROW = utf8.char(0xe0b0) -- for left-most cell (the one that is highlighted)
+local RIGHT_ARROW = utf8.char(0xe0b1) -- for intermediate cells
+local PROJECT_ICON = utf8.char(0xefce) -- for right-most cell (the one that has a different background)
+-- colors from NVIM lualine catpuccin theme
+local clr_accent_normal = "#89B4FA"
+local clr_accent_leader = "#FAB387"
+local clr_background = "#252536" -- catpuccin NVIM lualine bg color
+local clr_bg_dark = "#11111B" -- for right-most cell (this color is the bg color of wezterm tab bar)
+-- <<< Style elements >>>
+
 -- NOTE: Attempt to fix terminal name in fastfetch header
 -- https://wezfurlong.org/wezterm/config/lua/config/term.html
 config.term = "wezterm"
+config.default_workspace = "main"
 
 config.font = wezterm.font_with_fallback({ "CaskaydiaCove Nerd Font", "Hack Nerd Font", "Noto Color Emoji" })
 config.font_size = 16
@@ -24,10 +36,33 @@ config.window_padding = {
 	top = 20,
 	bottom = 20,
 }
-wezterm.on("update-right-status", function(window, pane)
-	-- TODO: Improve status bar visuals (workspace name indicator, is_leader_active)
-	-- https://github.com/wez/wezterm/issues/500#issuecomment-792202306
-	window:set_right_status(window:active_workspace())
+wezterm.on("update-status", function(window, pane)
+	window:set_right_status("")
+
+	local leader = "NORMAL"
+	local accent_clr = clr_accent_normal
+	if window:leader_is_active() then
+		leader = "LEADER"
+		accent_clr = clr_accent_leader
+	end
+
+	local elements = {
+		{ Background = { Color = clr_background } },
+		{ Text = " " },
+		{ Foreground = { Color = clr_background } },
+		{ Background = { Color = accent_clr } },
+		{ Text = " " .. leader .. " " },
+		{ Foreground = { Color = accent_clr } },
+		{ Background = { Color = clr_background } },
+		{ Text = SOLID_RIGHT_ARROW },
+		{ Text = " " .. PROJECT_ICON .. "  " .. window:active_workspace() .. " " },
+		"ResetAttributes",
+		{ Foreground = { Color = clr_background } },
+		{ Background = { Color = clr_bg_dark } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+
+	window:set_left_status(wezterm.format(elements))
 end)
 
 config.color_scheme = "Catppuccin Mocha"
@@ -42,6 +77,10 @@ config.color_scheme = "Catppuccin Mocha"
 -- config.color_scheme = "CustomCatppuccin"
 
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.colors = {
+	-- NOTE: Change cursor color when wezterm LEADER is active
+	compose_cursor = accent_leader,
+}
 -- TODO: setup cmd+N keybind to open new pane 'smartly'
 --       decide whether to do vsplit/hsplit depending on how many panes are currently open
 config.keys = {
