12ad7df4540ae6e62cfbf4ad1bed9e24744f59b7
Rabeez Riaz <ruoter123@gmail.com>
2024-12-03 16:44:53 +0400

    Wezterm custom tab bar with other config changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local sessionizer = require("sessionizer")
 local config = wezterm.config_builder()
 
@@ -8,6 +9,8 @@ local function basename(s)
 	-- Isolates name of program from full absolute path of executable
 	return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
+local SOLID_RIGHT_HALF_CIRCLE = utf8.char(0xe0b4) -- for left-most cell (the one that is highlighted)
+local SOLID_LEFT_HALF_CIRCLE = utf8.char(0xe0b6) -- for left-most cell (the one that is highlighted)
 local SOLID_RIGHT_ARROW = utf8.char(0xe0b0) -- for left-most cell (the one that is highlighted)
 local RIGHT_ARROW = utf8.char(0xe0b1) -- for intermediate cells
 local PROJECT_ICON = utf8.char(0xe601) -- for right-most cell (the one that has a different background)
@@ -23,9 +26,16 @@ local clr_bg_dark = "#181825" -- for right-most cell (this color is the bg color
 config.term = "wezterm"
 config.default_workspace = "main"
 
+config.use_ime = false -- Needed to ensure ctrl-based keymaps are passed to processes (eg. in nvim)
+config.max_fps = 120
+config.default_cursor_style = "SteadyBar"
 config.font = wezterm.font_with_fallback({ "CaskaydiaCove Nerd Font", "Hack Nerd Font", "Noto Color Emoji" })
 config.font_size = 16
 config.line_height = 1.2
+config.command_palette_font_size = 18
+-- These colors are copied from catppuccin palette in nvim quickfix list design
+config.command_palette_bg_color = "#13121D"
+config.command_palette_fg_color = "#C0CCF5"
 -- NOTE: Fully disabling tab bar also removes status bar indicators which contains active workspace
 -- config.enable_tab_bar = false
 -- https://github.com/wez/wezterm/issues/2082#issuecomment-1320967574
@@ -36,13 +46,39 @@ config.window_decorations = "RESIZE"
 config.initial_rows = 35
 config.initial_cols = 110
 config.window_padding = {
-	left = 20,
-	right = 20,
-	top = 20,
-	bottom = 20,
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5,
 }
 
+wezterm.on("window-config-reloaded", function(window, pane)
+	window:toast_notification("wezterm", "Configuration reloaded!", nil, 4000)
+end)
+
+-- TODO: this doesn't seem to actually update the tab title :(
+-- because my custom status bar update overwrites tab name to fg process always
+wezterm.on("augment-command-palette", function(window, pane)
+	return {
+		{
+			brief = "Rename tab",
+			icon = "md_rename_box",
+
+			action = act.PromptInputLine({
+				description = "Enter new name for tab",
+				-- initial_value = "My Tab Name",
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
+	}
+end)
+
 wezterm.on("update-status", function(window, pane)
+	-- Determine accent color based on if wezterm leader is active
 	local leader = "NORMAL"
 	local accent_clr = clr_accent_normal
 	if window:leader_is_active() then
@@ -52,36 +88,36 @@ wezterm.on("update-status", function(window, pane)
 
 	local workspaces = wezterm.mux:get_workspace_names()
 	local elements = {
+		-- Single element spacer on left edge
 		{ Background = { Color = clr_background } },
 		{ Text = " " },
 		"ResetAttributes",
 
+		-- Terminal input mode
 		{ Foreground = { Color = clr_background } },
 		{ Background = { Color = accent_clr } },
 		{ Text = " " .. leader .. " " },
 		"ResetAttributes",
 
+		-- Intermediate chevron and workspace info
 		{ Foreground = { Color = accent_clr } },
 		{ Background = { Color = clr_background } },
 		{ Text = SOLID_RIGHT_ARROW },
 		{ Text = " [" .. tostring(#workspaces) .. "] " .. PROJECT_ICON .. " " .. window:active_workspace() .. " " },
 		"ResetAttributes",
 
+		-- Right chevron terminal character
 		{ Foreground = { Color = clr_background } },
 		{ Background = { Color = clr_bg_dark } },
 		{ Text = SOLID_RIGHT_ARROW },
 	}
+
 	window:set_left_status(wezterm.format(elements))
 
 	elements = {}
-	for _, item in ipairs(window:mux_window():tabs_with_info()) do
-		if item.is_active then
-			table.insert(elements, { Foreground = { Color = clr_background } })
-			table.insert(elements, { Background = { Color = accent_clr } })
-		else
-			table.insert(elements, { Foreground = { Color = accent_clr } })
-			table.insert(elements, { Background = { Color = clr_background } })
-		end
+	tab_arr = window:mux_window():tabs_with_info()
+	for tab_index, item in ipairs(tab_arr) do
+		-- Ensure tab name correspond to foreground process name
 		local proc_name = "< TEMP >"
 		for _, pane_item in ipairs(item.tab:panes_with_info()) do
 			if pane_item.is_active then
@@ -89,10 +125,47 @@ wezterm.on("update-status", function(window, pane)
 				item.tab:set_title(basename(proc_name))
 			end
 		end
-		table.insert(elements, { Text = " " .. item.tab:get_title() .. " " })
+
+		-- Setup colors and write left half-circle
+		if not item.is_active then
+			table.insert(elements, { Foreground = { Color = clr_background } })
+			table.insert(elements, { Background = { Color = clr_bg_dark } })
+		else
+			table.insert(elements, { Foreground = { Color = accent_clr } })
+			table.insert(elements, { Background = { Color = clr_bg_dark } })
+		end
+		table.insert(elements, { Text = SOLID_LEFT_HALF_CIRCLE })
+
+		-- Setup colors and write tab name
+		if item.is_active then
+			table.insert(elements, { Foreground = { Color = clr_background } })
+			table.insert(elements, { Background = { Color = accent_clr } })
+		else
+			table.insert(elements, { Foreground = { Color = accent_clr } })
+			table.insert(elements, { Background = { Color = clr_background } })
+		end
+		table.insert(elements, { Text = item.tab:get_title() })
+
+		-- Setup colors and write right half-circle
+		if not item.is_active then
+			table.insert(elements, { Foreground = { Color = clr_background } })
+			table.insert(elements, { Background = { Color = clr_bg_dark } })
+		else
+			table.insert(elements, { Foreground = { Color = accent_clr } })
+			table.insert(elements, { Background = { Color = clr_bg_dark } })
+		end
+		table.insert(elements, { Text = SOLID_RIGHT_HALF_CIRCLE })
+
+		-- Spacing between tabs, except to right side of last tab
+		if tab_index ~= #tab_arr then
+			table.insert(elements, { Text = " " })
+		end
 	end
-	table.insert(elements, { Background = { Color = clr_background } })
+
+	-- Single element spacer on right edge
+	table.insert(elements, { Background = { Color = clr_bg_dark } })
 	table.insert(elements, { Text = " " })
+
 	window:set_right_status(wezterm.format(elements))
 end)
 
@@ -109,6 +182,11 @@ config.colors = {
 -- TODO: setup cmd+N keybind to open new pane 'smartly'
 --       decide whether to do vsplit/hsplit depending on how many panes are currently open
 config.keys = {
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCommandPalette,
+	},
 	{ key = "D", mods = "LEADER", action = wezterm.action.ShowDebugOverlay },
 	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
 	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
@@ -127,7 +205,7 @@ config.keys = {
 		}),
 	},
 	{ key = "h", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
-	{ key = "l", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "l", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
 	{
 		key = "n",
 		mods = "LEADER",
@@ -139,5 +217,10 @@ config.keys = {
 		action = wezterm.action.CloseCurrentTab({ confirm = true }),
 	},
 }
+-- TODO
+-- keybind LEADER+a -> m
+-- open text input to create new directory somewhere in programming root with user-specified name
+-- create a git repo here
+-- open this directory as a new workspace
 
 return config
