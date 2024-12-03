5fc04c9b49baf905306c7d5fbac087a61c1e8e82
Rabeez Riaz <ruoter123@gmail.com>
2024-12-03 16:45:52 +0400

    Scroll and window focus keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,12 @@ vim.g.maplocalleader = " "
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+vim.keymap.set({ "n", "v" }, "<space>", "<NOP>")
+vim.keymap.set({ "n", "v" }, "<CR>", "<NOP>")
+
+-- Black-hole register for character deletion
+vim.keymap.set("n", "x", '"_x')
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
@@ -18,10 +24,29 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-H>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-L>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-J>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-K>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "[t", "<cmd>bprevious<CR>", { desc = "Go to previous buffer/[t]ab" })
+vim.keymap.set("n", "]t", "<cmd>bnext<CR>", { desc = "Go to next buffer/[t]ab" })
 
 -- TODO:
--- shift-K / J to move visual selection up/down
+-- ]]/[[ to move to end/start of current scope (use treesitter??)
+-- alternate: perform visual selection e.g. vim (select inside function/method, then press o to go to beginning/end of selection)
+
+-- Preserve yank buffer after paste
+-- vim.keymap.set("n", "p", "pgvy")
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page [u]p and center cursor" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page [d]p and center cursor" })
+
+vim.keymap.set("n", "U", "<cmd>redo<CR>", { desc = "Redo" })
+
+vim.keymap.set("n", "<C-p>", "o<ESC>p", { desc = "[p]aste in next line" })
