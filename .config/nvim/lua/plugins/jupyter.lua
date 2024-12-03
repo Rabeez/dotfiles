30788209f2f97ffbc26c91bd3f9dd27921eee9a5
Rabeez Riaz <ruoter123@gmail.com>
2024-12-03 16:48:21 +0400

    Autoformat

--- .config/nvim/lua/plugins/jupyter.lua
+++ .config/nvim/lua/plugins/jupyter.lua
@@ -190,37 +190,37 @@ return {
 			-- note: the metadata is needed for Jupytext to understand how to parse the notebook.
 			-- if you use another language than Python, you should change it in the template.
 			local default_notebook = [[
-                          {
-                            "cells": [
-                             {
-                              "cell_type": "markdown",
-                              "metadata": {},
-                              "source": [
-                                ""
-                              ]
-                             }
-                            ],
-                            "metadata": {
-                             "kernelspec": {
-                              "display_name": "Python 3",
-                              "language": "python",
-                              "name": "python3"
-                             },
-                             "language_info": {
-                              "codemirror_mode": {
-                                "name": "ipython"
-                              },
-                              "file_extension": ".py",
-                              "mimetype": "text/x-python",
-                              "name": "python",
-                              "nbconvert_exporter": "python",
-                              "pygments_lexer": "ipython3"
-                             }
-                            },
-                            "nbformat": 4,
-                            "nbformat_minor": 5
-                          }
-                        ]]
+			  {
+			    "cells": [
+			     {
+			      "cell_type": "markdown",
+			      "metadata": {},
+			      "source": [
+				""
+			      ]
+			     }
+			    ],
+			    "metadata": {
+			     "kernelspec": {
+			      "display_name": "Python 3",
+			      "language": "python",
+			      "name": "python3"
+			     },
+			     "language_info": {
+			      "codemirror_mode": {
+				"name": "ipython"
+			      },
+			      "file_extension": ".py",
+			      "mimetype": "text/x-python",
+			      "name": "python",
+			      "nbconvert_exporter": "python",
+			      "pygments_lexer": "ipython3"
+			     }
+			    },
+			    "nbformat": 4,
+			    "nbformat_minor": 5
+			  }
+			]]
 
 			local function new_notebook(filename)
 				local path = filename .. ".ipynb"
