e99b5a294a94c7a1fd113a3b4c0b22d09a6b11bd
Rabeez Riaz <ruoter123@gmail.com>
2024-12-04 18:10:48 +0400

    Attempt to improve sketchybar aerospace interaction

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -4,8 +4,15 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 source "$CONFIG_DIR/colors.sh"
 
+#  aerospace list-windows --workspace 4 | rg -q ".*" && echo "•" || echo " "
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-	sketchybar --set "$NAME" label.color="$MAUVE"
+	sketchybar --set "$NAME" label.color="$MAUVE2"
 else
+	# IS_NONEMPTY=$(aerospace list-windows --workspace "$1" | rg -q ".*" && echo "•" || echo "")
+	# if [[ "$IS_NONEMPTY" == "•" ]]; then
 	sketchybar --set "$NAME" label.color="$WHITE"
+	# else
+	# 	sketchybar --set "$NAME" label.color="$GREY"
+	# fi
 fi
