f866616407ab7cf4beec009792a844e7268ec111
Rabeez Riaz <ruoter123@gmail.com>
2024-12-08 23:59:05 +0400

    Add leetcode and toggleterm

---
+++ .config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			--[[ things you want to change go here]]
+			open_mapping = [[<c-\>]],
+			direction = "float",
+			float_opts = {
+				border = "rounded",
+			},
+		},
+	},
+}
