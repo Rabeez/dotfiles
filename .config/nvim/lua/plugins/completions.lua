af486a1644edf8d908961fd2b92dabff4551a836
Rabeez Riaz <ruoter123@gmail.com>
2025-01-06 13:41:59 +0400

    Better cmp UI

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -31,6 +31,14 @@ return {
 		config = function()
 			local cmp = require("cmp")
 			local lspkind = require("lspkind")
+			local lspkind_cmp_format = lspkind.cmp_format({
+				mode = "symbol_text",
+				maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+				-- can also be a function to dynamically calculate max width such as
+				-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+				ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+				show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+			})
 			require("luasnip.loaders.from_vscode").lazy_load()
 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
@@ -69,13 +77,10 @@ return {
 					{ name = "path", max_item_count = 5 },
 				}),
 				formatting = {
-					format = lspkind.cmp_format({
-						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-						-- can also be a function to dynamically calculate max width such as
-						-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-					}),
+					format = function(entry, vim_item)
+						vim_item.menu = entry.source.name
+						return lspkind.cmp_format({})(entry, vim_item)
+					end,
 				},
 			})
 
