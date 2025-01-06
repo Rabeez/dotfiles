7e55558f3268edb8c6ae1dc96c317d1605b5ce88
Rabeez Riaz <ruoter123@gmail.com>
2025-01-06 18:57:24 +0400

    Completions icons

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -33,6 +33,7 @@ return {
 			local lspkind = require("lspkind")
 			local lspkind_cmp_format = lspkind.cmp_format({
 				mode = "symbol_text",
+				preset = "codicons",
 				maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 				-- can also be a function to dynamically calculate max width such as
 				-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
