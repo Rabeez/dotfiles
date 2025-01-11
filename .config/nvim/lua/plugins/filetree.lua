43202f30746ccc613679e23965da63ea3cb85209
Rabeez Riaz <ruoter123@gmail.com>
2025-01-11 18:57:40 +0400

    Swap Yazi plugin

--- .config/nvim/lua/plugins/filetree.lua
+++ .config/nvim/lua/plugins/filetree.lua
@@ -43,37 +43,49 @@ return {
 	-- 			{ desc = "Toggle Neotree panel", silent = true })
 	-- 	end
 	-- },
+	-- {
+	-- 	-- NOTE: this one randomly broke
+	-- 	-- "mikavilpas/yazi.nvim",
+	-- 	event = "VeryLazy",
+	-- 	keys = {
+	-- 		-- 👇 in this section, choose your own keymappings!
+	-- 		{
+	-- 			"<leader>ef",
+	-- 			"<cmd>Yazi<cr>",
+	-- 			desc = "[E]xplorer: Open yazi at the current [f]ile",
+	-- 		},
+	-- 		{
+	-- 			-- Open in the current working directory
+	-- 			"<leader>ew",
+	-- 			"<cmd>Yazi cwd<cr>",
+	-- 			desc = "[E]xplorer: Open yazi in [w]orking directory",
+	-- 		},
+	-- 		-- {
+	-- 		-- 	-- NOTE: this requires a version of yazi that includes
+	-- 		-- 	-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+	-- 		-- 	"<c-up>",
+	-- 		-- 	"<cmd>Yazi toggle<cr>",
+	-- 		-- 	desc = "Resume the last yazi session",
+	-- 		-- },
+	-- 	},
+	-- 	---@type YaziConfig
+	-- 	opts = {
+	-- 		-- NOTE: If using this then neotree `hijack_netrw_behavior` has to be `disabled`
+	-- 		open_for_directories = true,
+	-- 		keymaps = {
+	-- 			show_help = "<f1>",
+	-- 		},
+	-- 	},
+	-- },
 	{
-		"mikavilpas/yazi.nvim",
-		event = "VeryLazy",
-		keys = {
-			-- 👇 in this section, choose your own keymappings!
-			{
-				"<leader>ef",
-				"<cmd>Yazi<cr>",
-				desc = "[E]xplorer: Open yazi at the current [f]ile",
-			},
-			{
-				-- Open in the current working directory
-				"<leader>ew",
-				"<cmd>Yazi cwd<cr>",
-				desc = "[E]xplorer: Open yazi in [w]orking directory",
-			},
-			-- {
-			-- 	-- NOTE: this requires a version of yazi that includes
-			-- 	-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-			-- 	"<c-up>",
-			-- 	"<cmd>Yazi toggle<cr>",
-			-- 	desc = "Resume the last yazi session",
-			-- },
+		"DreamMaoMao/yazi.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
 		},
-		---@type YaziConfig
-		opts = {
-			-- NOTE: If using this then neotree `hijack_netrw_behavior` has to be `disabled`
-			open_for_directories = true,
-			keymaps = {
-				show_help = "<f1>",
-			},
+
+		keys = {
+			{ "<leader>ee", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
 		},
 	},
 }
