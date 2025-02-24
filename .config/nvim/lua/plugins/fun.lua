f521e7018adb1d062d547f5e56f0f00fc6a9f087
Rabeez Riaz <ruoter123@gmail.com>
2025-02-24 16:57:52 +0400

    Add nvim cellular automata plugin

---
+++ .config/nvim/lua/plugins/fun.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "eandrju/cellular-automaton.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>uc", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Make it Rain" })
+    end,
+  },
+}
