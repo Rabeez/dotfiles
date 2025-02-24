42876f3f322705dec34aa07877959c2484493c21
Rabeez Riaz <ruoter123@gmail.com>
2025-02-24 23:09:14 +0400

    Clean nvim plugins

--- .config/nvim/lua/plugins/linting_formatting.lua
+++ .config/nvim/lua/plugins/linting_formatting.lua
@@ -7,6 +7,15 @@ return {
     end,
   },
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    -- TODO:
+    -- add keymaps for folding? and moving ([h, ]h) to headers (see how to do levels of headers?)
+  },
+  {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {},
