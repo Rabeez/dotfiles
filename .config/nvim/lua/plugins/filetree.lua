cc7a45698f5c10a6770766f8cfae2ad3a0ef9723
Rabeez Riaz <ruoter123@gmail.com>
2025-05-14 19:08:32 +0400

    Setup neotree file explorer for nvim

--- .config/nvim/lua/plugins/filetree.lua
+++ .config/nvim/lua/plugins/filetree.lua
@@ -9,40 +9,55 @@ return {
   -- 		require("oil").setup()
   -- 	end,
   -- },
-  -- {
-  -- 	"nvim-neo-tree/neo-tree.nvim",
-  -- 	branch = "v3.x",
-  -- 	dependencies = {
-  -- 		"nvim-lua/plenary.nvim",
-  -- 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-  -- 		"MunifTanjim/nui.nvim",
-  -- 		"3rd/image.nvim",     -- Optional image support in preview window: See `# Preview Mode` for more information
-  -- 	},
-  -- 	config = function()
-  -- 		-- TODO: check if this shows untracked git files in different color
-  -- 		-- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/353
-  -- 		require("neo-tree").setup({
-  -- 			filesystem = {
-  -- 				filtered_items = {
-  -- 					visible = true
-  -- 				},
-  -- 				follow_current_file = {
-  -- 					enabled = true, -- This will find and focus the file in the active buffer every time
-  -- 					--               -- the current file is changed while the tree is open.
-  -- 					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-  -- 				},
-  -- 			},
-  -- 			window = {
-  -- 				-- toggle = true,
-  -- 				position = "right",
-  -- 			},
-  -- 			hijack_netrw_behavior = "disabled"
-  -- 		})
-  -- 		-- TODO: <C-b> has a conflict within neotree so it doesn't close an opened panel
-  -- 		vim.keymap.set("n", "<leader>bb", "<Cmd>Neotree toggle last<CR>",
-  -- 			{ desc = "Toggle Neotree panel", silent = true })
-  -- 	end
-  -- },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      -- TODO: check if this shows untracked git files in different color
+      -- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/353
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = true,
+          },
+          follow_current_file = {
+            enabled = true, -- This will find and focus the file in the active buffer every time
+            --               -- the current file is changed while the tree is open.
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          },
+        },
+        window = {
+          -- toggle = true,
+          position = "right",
+        },
+        hijack_netrw_behavior = "disabled",
+        sources = {
+          "filesystem",
+          "buffers",
+          "document_symbols",
+        },
+      })
+      -- TODO: <C-b> has a conflict within neotree so it doesn't close an opened panel
+      vim.keymap.set(
+        "n",
+        "<leader>et",
+        "<Cmd>Neotree toggle filesystem<CR>",
+        { desc = "Toggle Neotree panel", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>ls",
+        "<Cmd>Neotree toggle document_symbols<CR>",
+        { desc = "[L]SP: Toggle document [s]ymbols", silent = true }
+      )
+    end,
+  },
   -- {
   -- 	-- NOTE: this one randomly broke
   -- 	-- "mikavilpas/yazi.nvim",
