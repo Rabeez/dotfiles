36b6352eb34daf922fde8cf2ff130e92e8007732
Rabeez Riaz <ruoter123@gmail.com>
2025-05-27 17:16:56 +0400

    Remove un-needed color schemes

--- .config/nvim/lua/plugins/color_schemes.lua
+++ .config/nvim/lua/plugins/color_schemes.lua
@@ -56,79 +56,60 @@ return {
       })
     end,
   },
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("kanagawa").setup({
-        compile = false, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = false },
-        functionStyle = {},
-        keywordStyle = { italic = false },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors) -- add/modify highlights
-          return {}
-        end,
-        theme = "wave", -- Load "wave" theme
-        background = { -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus",
-        },
-      })
-    end,
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("nightfox").setup({
-        options = {
-          styles = {
-            keywords = "bold",
-          },
-        },
-      })
-    end,
-  },
-  -- NOTE: gruvbox themes have single entry in neovim
-  -- Dark/light is controlled by `vim.o.background=dark`
-  -- which messes with wezterm sync etc
   -- {
-  --   "sainnhe/gruvbox-material",
-  --   lazy = false,
-  --   priority = 1000,
-  -- },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-      },
-    },
-  },
-  -- NOTE: This is sadly same as gruvbox
-  -- {
-  --   "neanias/everforest-nvim",
+  --   "rebelot/kanagawa.nvim",
   --   lazy = false,
   --   priority = 1000,
   --   config = function()
-  --     require("everforest").setup({
-  --       background = "hard",
+  --     require("kanagawa").setup({
+  --       compile = false, -- enable compiling the colorscheme
+  --       undercurl = true, -- enable undercurls
+  --       commentStyle = { italic = false },
+  --       functionStyle = {},
+  --       keywordStyle = { italic = false },
+  --       statementStyle = { bold = true },
+  --       typeStyle = {},
+  --       transparent = false, -- do not set background color
+  --       dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  --       terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  --       colors = { -- add/modify theme and palette colors
+  --         palette = {},
+  --         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  --       },
+  --       overrides = function(colors) -- add/modify highlights
+  --         return {}
+  --       end,
+  --       theme = "wave", -- Load "wave" theme
+  --       background = { -- map the value of 'background' option to a theme
+  --         dark = "wave", -- try "dragon" !
+  --         light = "lotus",
+  --       },
   --     })
   --   end,
+  -- },
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("nightfox").setup({
+  --       options = {
+  --         styles = {
+  --           keywords = "bold",
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     styles = {
+  --       comments = { italic = false },
+  --       keywords = { italic = false },
+  --     },
+  --   },
   -- },
 }
