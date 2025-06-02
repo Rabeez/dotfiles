016929055562f95c548cb27ec24082c633a94858
Rabeez Riaz <ruoter123@gmail.com>
2025-06-03 01:08:58 +0400

    Tweak flash nvim config

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -144,11 +144,29 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
-  -- stylua: ignore
-  keys = {
-    { "<C-f>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-  },
+    opts = {
+      modes = {
+        search = { enabled = false },
+        char = { enabled = false },
+      },
+    },
+    keys = {
+      {
+        "<C-f>",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+    },
   },
 }
