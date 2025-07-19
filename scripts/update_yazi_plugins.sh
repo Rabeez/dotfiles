a0caf1103ea1d2b4466f31b5bed7319d33bf2cee
Rabeez Riaz <ruoter123@gmail.com>
2025-07-19 21:20:48 +0500

    Improve help message for yazi update script

--- scripts/update_yazi_plugins.sh
+++ scripts/update_yazi_plugins.sh
@@ -5,11 +5,13 @@ if [[ "$1" == "-f" ]]; then
     rm -rf .config/yazi/plugins
     ya pkg upgrade
 else
-    echo "Run with -f to execute commands."
-    echo "Before this, make sure that no 'manual' plugins exist in yazi config plugins directory"
+    echo "Run with -f to execute commands!!!"
+    echo ""
+    echo "Before this, make sure that no *manual* plugins exist in yazi config plugins directory"
     echo "    such as those from yazi docs (tips page)."
+    echo "These will not have an entry in yazi package.toml"
+    echo ""
     echo "You can run these commands to verify manually:"
     echo "    'ls .config/yazi/plugins'"
-    echo "    'cat .config/yazi/package.toml'"
-    echo "These will not have an entry in yazi package.toml"
+    echo "    'cat .config/yazi/package.toml | rg use | sed \"s .*/  \" | tr --delete '\"' | sed \"s/plugins://\" | xargs -I {} echo \"{}.yazi\" | sort'"
 fi
