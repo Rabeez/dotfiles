1b6f47b0fa332764e8f8d6cff2ad447641a334e1
Rabeez Riaz <ruoter123@gmail.com>
2025-07-19 05:10:18 +0500

    Update yazi plugins

--- scripts/update_yazi_plugins.sh
+++ scripts/update_yazi_plugins.sh
@@ -3,11 +3,13 @@
 if [[ "$1" == "-f" ]]; then
     echo "Running commands..."
     rm -rf .config/yazi/plugins
-    ya pack -u
-    command3
+    ya pkg upgrade
 else
     echo "Run with -f to execute commands."
     echo "Before this, make sure that no 'manual' plugins exist in yazi config plugins directory"
     echo "    such as those from yazi docs (tips page)."
+    echo "You can run these commands to verify manually:"
+    echo "    'ls .config/yazi/plugins'"
+    echo "    'cat .config/yazi/package.toml'"
     echo "These will not have an entry in yazi package.toml"
 fi
