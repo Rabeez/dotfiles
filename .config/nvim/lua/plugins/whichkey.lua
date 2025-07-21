bf3c7fb12d9d80b0774fc5ea5b0611c137d84287
Rabeez Riaz <ruoter123@gmail.com>
2025-07-22 04:37:55 +0500

    Update whichkey label

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -24,7 +24,7 @@ return {
       { "<leader>g", group = "[G]it", icon = { icon = "󰊢", color = "red" } },
       { "<leader>e", group = "File [e]xplorer", icon = { icon = "", color = "azure" } },
       { "<leader>d", group = "[D]ebugger", icon = { icon = "", color = "purple" } },
-      { "<leader>f", group = "Telescope fuzzy [f]inder", icon = { icon = "", color = "azure" } },
+      { "<leader>f", group = "fuzzy [f]inder", icon = { icon = "", color = "azure" } },
       { "<leader>h", group = "[H]arpoon", icon = { icon = "", color = "azure" } },
       { "<leader>m", group = "[M]olten", icon = { icon = "󰺿", color = "purple" } },
       { "<leader>a", group = "[A]I", icon = { icon = "", color = "yellow" } },
