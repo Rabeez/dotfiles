d976f44503ccae534ed5fd1ebf0e4e7fd32bcf46
Rabeez Riaz <ruoter123@gmail.com>
2025-10-09 21:07:26 +0300

    Disable notes

--- .config/nvim/lua/plugins/notes.lua
+++ .config/nvim/lua/plugins/notes.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "jellydn/my-note.nvim",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    keys = {
-      {
-        "<leader>un",
-        "<cmd>MyNote<cr>",
-        desc = "Open [n]ote",
-      },
-    },
-    opts = {
-      files = {
-        -- Using the parent .git folder as the current working directory
-        cwd = function()
-          local bufPath = vim.api.nvim_buf_get_name(0)
-          local cwd = require("lspconfig").util.root_pattern(".git")(bufPath)
-
-          return cwd
-        end,
-      },
-    },
-  },
+  -- {
+  --   "jellydn/my-note.nvim",
+  --   dependencies = { "MunifTanjim/nui.nvim" },
+  --   keys = {
+  --     {
+  --       "<leader>un",
+  --       "<cmd>MyNote<cr>",
+  --       desc = "Open [n]ote",
+  --     },
+  --   },
+  --   opts = {
+  --     files = {
+  --       -- Using the parent .git folder as the current working directory
+  --       cwd = function()
+  --         local bufPath = vim.api.nvim_buf_get_name(0)
+  --         local cwd = require("lspconfig").util.root_pattern(".git")(bufPath)
+  --
+  --         return cwd
+  --       end,
+  --     },
+  --   },
+  -- },
 }
