3f54f7e8456aefe14a64eecf7aae98e66db4c32c
Rabeez Riaz <ruoter123@gmail.com>
2025-10-13 04:31:53 +0300

    Improve nvim transparency support

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -416,8 +416,14 @@ return {
   },
   {
     "j-hui/fidget.nvim",
-    opts = {
-      -- options
-    },
+    config = function()
+      require("fidget").setup({
+        notification = {
+          window = {
+            winblend = 0,
+          },
+        },
+      })
+    end,
   },
 }
