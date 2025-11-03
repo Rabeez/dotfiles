c9bcd067c5a71f0b3cdcdc4000dbd29b33d18de2
Rabeez Riaz <ruoter123@gmail.com>
2025-11-03 16:34:11 +0200

    Cleanup

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -138,32 +138,5 @@ return {
     config = true,
   },
   {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {
-      modes = {
-        search = { enabled = false },
-        char = { enabled = false },
-      },
-    },
-    keys = {
-      {
-        "<C-f>",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote()
-        end,
-        desc = "Remote Flash",
-      },
-    },
   },
 }
--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -112,4 +112,33 @@ return {
       end
     end,
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        search = { enabled = false },
+        char = { enabled = false },
+      },
+    },
+    keys = {
+      {
+        "<C-f>",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+    },
+  },
 }
